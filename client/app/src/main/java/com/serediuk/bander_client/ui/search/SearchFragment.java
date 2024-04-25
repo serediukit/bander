e93f59c4aa4c0dc8f6aee960f01562679448b525
Valentyn Serediuk <serediukit@gmail.com>
2024-04-26 01:33:53 +0300

    update ui

---
+++ client/app/src/main/java/com/serediuk/bander_client/ui/search/SearchFragment.java
@@ -0,0 +1,38 @@
+package com.serediuk.bander_client.ui.search;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.serediuk.bander_client.databinding.FragmentChatsBinding;
+import com.serediuk.bander_client.databinding.FragmentSearchBinding;
+
+public class SearchFragment extends Fragment {
+
+    private FragmentSearchBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        SearchViewModel homeViewModel =
+                new ViewModelProvider(this).get(SearchViewModel.class);
+
+        binding = FragmentSearchBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textSearch;
+        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
