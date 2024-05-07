fbd1b70b302ea3b6413169b2e17b40787d99f2fd
Valentyn Serediuk <serediukit@gmail.com>
2024-05-08 01:21:56 +0300

    add birthday picker

--- client/app/src/main/java/com/serediuk/bander_client/RegistrationActivity.java
+++ client/app/src/main/java/com/serediuk/bander_client/RegistrationActivity.java
@@ -2,6 +2,8 @@ package com.serediuk.bander_client;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
@@ -11,6 +13,12 @@ import android.widget.Toast;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.serediuk.bander_client.auth.AuthProvider;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
 
 public class RegistrationActivity extends AppCompatActivity {
     private EditText mEmail, mPassword;
@@ -34,6 +42,7 @@ public class RegistrationActivity extends AppCompatActivity {
         };
 
         Button mRegistrationButton = findViewById(R.id.registration_button);
+        Button mLoginButton = findViewById(R.id.login_button);
 
         mEmail = findViewById(R.id.emailEditText);
         mPassword = findViewById(R.id.passwordEditText);
@@ -47,6 +56,14 @@ public class RegistrationActivity extends AppCompatActivity {
                 }
             });
         });
+
+        mLoginButton.setOnClickListener(v -> {
+            Intent intent = new Intent(RegistrationActivity.this, LoginRegisterActivity.class);
+            startActivity(intent);
+            finish();
+        });
+
+        transformIntoDatePicker(findViewById(R.id.dateEditText), this, "MM/dd/yyyy", new Date());
     }
 
     @Override
@@ -59,5 +76,33 @@ public class RegistrationActivity extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
         authProvider.removeAuthStateListener(firebaseAuthStateListener);
+    }
+
+    private static void transformIntoDatePicker(EditText editText, Context context, String format, Date maxDate) {
+        editText.setFocusableInTouchMode(false);
+        editText.setClickable(true);
+        editText.setFocusable(false);
+
+        final Calendar myCalendar = Calendar.getInstance();
+        final DatePickerDialog.OnDateSetListener datePickerOnDataSetListener =
+                (view, year, monthOfYear, dayOfMonth) -> {
+                    myCalendar.set(Calendar.YEAR, year);
+                    myCalendar.set(Calendar.MONTH, monthOfYear);
+                    myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                    SimpleDateFormat sdf = new SimpleDateFormat(format, Locale.UK);
+                    editText.setText(sdf.format(myCalendar.getTime()));
+                };
+
+        editText.setOnClickListener(v -> {
+            DatePickerDialog datePickerDialog = new DatePickerDialog(
+                    context, datePickerOnDataSetListener, myCalendar
+                    .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                    myCalendar.get(Calendar.DAY_OF_MONTH)
+            );
+            if (maxDate != null) {
+                datePickerDialog.getDatePicker().setMaxDate(maxDate.getTime());
+            }
+            datePickerDialog.show();
+        });
     }
 }
