4104cfc31efdbacd8c43a05612c78d528edf137b
Valentyn Serediuk <serediukit@gmail.com>
2024-05-23 08:46:20 +0300

    update Home fragment

--- client/app/src/main/java/com/serediuk/bander_client/ui/home/HomeFragment.java
+++ client/app/src/main/java/com/serediuk/bander_client/ui/home/HomeFragment.java
@@ -14,19 +14,16 @@ import com.serediuk.bander_client.databinding.FragmentHomeBinding;
 import com.serediuk.bander_client.model.dao.*;
 
 public class HomeFragment extends Fragment {
+    HomeViewModel homeViewModel;
 
     private FragmentHomeBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
+        homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
 
         
 
