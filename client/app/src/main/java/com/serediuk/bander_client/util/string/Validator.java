9e11f10f3b2073fb3bc708f005d842ec6ab1ab45
Valentyn Serediuk <serediukit@gmail.com>
2024-06-10 06:54:46 +0300

    add .apk file

--- client/app/src/main/java/com/serediuk/bander_client/util/string/Validator.java
+++ client/app/src/main/java/com/serediuk/bander_client/util/string/Validator.java
@@ -7,11 +7,8 @@ import java.util.regex.Pattern;
 
 public class Validator {
     private static final String EMAIL_PATTERN = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
-
-    // Regular expression pattern for password validation
     private static final String PASSWORD_PATTERN = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$";
 
-    // Email validator method
     public static boolean isValidEmail(String email) {
         Pattern pattern = Pattern.compile(EMAIL_PATTERN);
         Matcher matcher = pattern.matcher(email);
@@ -19,7 +16,6 @@ public class Validator {
         return matcher.matches();
     }
 
-    // Password validator method
     public static boolean isValidPassword(String password) {
         Pattern pattern = Pattern.compile(PASSWORD_PATTERN);
         Matcher matcher = pattern.matcher(password);
