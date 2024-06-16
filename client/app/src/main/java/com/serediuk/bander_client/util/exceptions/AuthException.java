6863592703ed3660b1f18f6dfdd3d80bd440f371
Valentyn Serediuk <serediukit@gmail.com>
2024-06-17 02:15:59 +0300

    fix password message

--- client/app/src/main/java/com/serediuk/bander_client/util/exceptions/AuthException.java
+++ client/app/src/main/java/com/serediuk/bander_client/util/exceptions/AuthException.java
@@ -2,7 +2,7 @@ package com.serediuk.bander_client.util.exceptions;
 
 public class AuthException extends Exception {
     public AuthException() {
-        super("Illegal credentials");
+        super("Illegal credentials. Password should be at least 8 characters long, contains at least 1 character and 1 digit");
     }
 
     public AuthException(String errorMessage) {
